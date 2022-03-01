2028b24f1c738270cb92efd50c443bfd32a75ef8
beamsocute <98265302+beamsocute@users.noreply.github.com>
2022-03-02 00:19:33 +0700

    Create font.php

---
+++ font.php
@@ -0,0 +1,26 @@
+<html>
+  <head>
+    <title>การกำหนดรูปแบบตัวอักษร</title>
+    <style>
+      @font-face{
+        font-family: "MyFont1";
+        src: url("baozi.ttf");
+      }
+      @font-face{
+        font-family: "MyFont2";
+        src: url("baozi.ttf");
+      }
+      #font1{
+        font-family: "MyFont1";
+      }
+      #font2{
+        font-family: "MyFont2";
+      }
+    </style>
+  </head>
+  <body>
+    <h1 id="font1">นางสาวดารุณี สิงห์บุญมา เลขที่ 8 ห้อง ม.4/14 เลขประจำตัว 88888</h1>
+    <h1 id="font2">นางสาวดารุณี สิงห์บุญมา เลขที่ 8 4ห้อง ม.4/14 เลขประจำตัว 88888</h1>
+  </body>
+</html>
+© 2022 GitHub
