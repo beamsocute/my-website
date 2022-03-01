9f18cc629eff04c184a6a0980f780444d41c2427
beamsocute <98265302+beamsocute@users.noreply.github.com>
2022-03-02 00:27:22 +0700

    Update font.php

--- font.php
+++ font.php
@@ -4,11 +4,11 @@
     <style>
       @font-face{
         font-family: "MyFont1";
-        src: url("baozi.ttf");
+        src: url("MiPancake.ttf");
       }
       @font-face{
         font-family: "MyFont2";
-        src: url("baozi.ttf");
+        src: url("SOVc_PhaKhawMa.ttf");
       }
       #font1{
         font-family: "MyFont1";
