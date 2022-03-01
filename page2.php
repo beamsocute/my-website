d8f508f766959fb6fd016d4832f17d9f9b350a91
beamsocute <98265302+beamsocute@users.noreply.github.com>
2022-03-02 00:07:22 +0700

    Create page2.php

---
+++ page2.php
@@ -0,0 +1,24 @@
+<html>
+	<head>
+		<title>การตกแต่งเว็บไซต์ด้วย CSS</title>
+		<style>
+			#box1{
+				width: 800px;
+				height: 350px;
+			}
+			.box{
+				background-color: a4e6ff;
+			}
+			body{
+                                background-color: ffbaff;
+			}
+			h1{
+				color: fbff84;      
+		</style>
+	</head>
+	<body>
+		<div id="box1" class="box">
+			<h1>นางสาวดารุนี สิงห์บุญมา ชั้น 4/13 เลขที่ 8</h1>
+		</div>
+	</body>
+</html>
